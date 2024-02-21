5225651bb5504fe1dd48df3c72ac7c99e63e2556
varunboppudi <2100031104@kluniversity.in>
2024-02-21 16:40:35 +0530

    Patterns in java

--- src/Main.java
+++ src/Main.java
@@ -78,7 +78,7 @@ public class Main {
     {
         for(int i=1;i<=n;i++) {
 
-            for (int j = 1; j < n - i; j++)
+            for (int j = 1; j <= n - i; j++)
             {
                 System.out.print(" ");
             }
@@ -86,7 +86,7 @@ public class Main {
             {
                 System.out.print("*");
             }
-            for (int j = 1; j < n - i; j++)
+            for (int j = 1; j <= n - i; j++)
             {
                 System.out.print(" ");
             }
